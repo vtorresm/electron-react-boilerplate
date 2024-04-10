340aed4367a5d944ec5981eecb104273280287c6
Victor Torres <vtorremo@emeal.nttdata.com>
2024-04-09 23:29:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron/main';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//let mainWindow;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  //mainWindow.loadURL('http://localhost:3000');
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
